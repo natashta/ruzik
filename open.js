dfcc134d80d47495309316727d36377f606b7ba4
natashta <natashta@gmail.com>
2020-03-31 12:45:42 +0300

    add_provereno_script

--- open.js
+++ open.js
@@ -1,14 +1,22 @@
-var button = document.querySelector(".button--red");
-var popup = document.querySelector(".modal__popup");
-var parent = document.querySelector(".modal-parent");
-var closer = document.querySelector(".modal__close");
+let form = document.querySelector("form");
+let button = form.querySelector(".button--red");
+let input = form.querySelector("input");
+let popup = document.querySelector(".modal__popup");
+let parent = document.querySelector(".modal-parent");
+let closer = document.querySelector(".modal__close");
 
 console.log(parent, popup);
 
 button.addEventListener("click", function(evt) {
     evt.preventDefault();
+    console.log(input.value);
     parent.classList.add("modal-show");
     popup.classList.add("modal-show");
+
+    provereno.check_code({ code: input.value.trim()}, function(result) {
+        console.log(result.data);
+    });
+            return false;
 });
 
 closer.addEventListener("click", function(evt) {
